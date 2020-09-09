f335596ecd87f8a49c2f99f99e22c07a0c468455
ssamuk <samukaijansinkko@gmail.com>
2020-09-09 15:48:20 +0300

    Osa 2 puhelinluettelo
    -File management
    -Also a checkpoint before going to filtering and moving things to components

--- osa-2/puhelinluettelo/src/index.js
+++ osa-2/puhelinluettelo/src/index.js
@@ -1,32 +1,80 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import React, { useState } from 'react'
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2020-01-10T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2020-01-10T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2020-01-10T19:20:14.298Z',
-    important: true
+  
+  const addNew = (event) => {
+    event.preventDefault()
+    const newObject = {
+      name: newName,
+      number: newNumber
+    }
+    let n = true;
+    for (let i = 0; i < persons.length; i++) {
+      if(persons[i].name === newObject.name){
+        n = false
+      }
+    }
+    if(n === true){
+      setPersons(persons.concat(newObject))
+        setNewName('')
+        setNewNumber('')
+    }
+    else{
+      alert(`Phonebook allready contains name: ${newObject.name}. We clear text field for you :)`)
+      setNewName('')
+      setNewNumber('')
+    }
   }
-]
+  
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>Filter shown with<input></input></div>
 
 
+      <div><h2>Add new</h2></div>
+      <form onSubmit={addNew}>
+        <div>
+          name:
+          <input value={newName} onChange={handleNoteChange}/>
+        </div>  
 
-ReactDOM.render(
-  <App notes={notes} />,
-  document.getElementById('root')
-)
+        <div>
+          phone:
+          <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+
+        <div><button type="submit">add</button></div>
+      </form>
+    
+      <h2>Numbers</h2>
+      <ul>
+            {persons.map(persons =>
+            <li key={persons.name}>{persons.name} {persons.number}</li>)}
+      </ul>
+    </div>
+  )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
