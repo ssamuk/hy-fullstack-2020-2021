b2bdd35713ade3cea6cb7f39d7afd6dbb16e631e
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-03 15:41:45 +0200

    Osa2 pakolliset tehtävät tehty. Palaan myöhemmin vapaaehtoisiin. Osa3 houkuttelee tällä hetkellä liikaa :D

--- osa-2/puhelinluettelo/src/App.js
+++ osa-2/puhelinluettelo/src/App.js
@@ -10,13 +10,29 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-
+  const [notification, setNotification] = useState(null)
+  
+  
   useEffect(() => {
-    personService.getAll()
+    personService
+    .getAll()
     .then(Persons => {
       setPersons(Persons)
     })
   }, [])
+
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
 
 
   const searchTermChange = event => {
@@ -45,7 +61,10 @@ const App = () => {
         setNewNumber('')
     }
     else{
-      alert(`Phonebook allready contains name: ${newObject.name}. We clear text field for you :)`)
+      setNotification(`ALERT! '${newObject.name}' is allready in phonebook`)
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
       setNewName('')
       setNewNumber('')
     }
@@ -76,6 +95,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification}/>
       <Filter
       value={searchTerm}
       onChange={searchTermChange}/>
