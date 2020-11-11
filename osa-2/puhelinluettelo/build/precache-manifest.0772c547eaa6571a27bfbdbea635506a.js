349ee6dd4e080588006fc10dd8948a60f08d5861
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-11 20:49:34 +0200

    checkpoint

---
+++ osa-2/puhelinluettelo/build/precache-manifest.0772c547eaa6571a27bfbdbea635506a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5824ad376f04971cb5fee1556d2990df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bb57fd7aefe46ee936f",
+    "url": "/static/css/main.48721ff5.chunk.css"
+  },
+  {
+    "revision": "d982e405c25a88eaf9b0",
+    "url": "/static/js/2.541a58d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.541a58d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb57fd7aefe46ee936f",
+    "url": "/static/js/main.6b21beb5.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  }
+]);
