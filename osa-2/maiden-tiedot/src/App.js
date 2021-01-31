1dd2a402b2a553c0a15590a2c9e1f24118e52ed5
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2021-01-31 23:19:42 +0200

    Maiden tiedot loppuun

--- osa-2/maiden-tiedot/src/App.js
+++ osa-2/maiden-tiedot/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+const Input = () => {
+  return(
+    <div>
+    <p>Find countries</p>
+    <input></input>
     </div>
-  );
+  )
+}
+
+
+
+
+
+
+ return (
+   <div>
+    <Input/>
+   </div>
+ )
 }
 
 export default App;
