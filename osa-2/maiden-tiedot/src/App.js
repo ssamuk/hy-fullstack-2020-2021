c27058f03c100afd3db9d84c497819a248f73a99
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2021-02-01 00:09:39 +0200

    --

--- osa-2/maiden-tiedot/src/App.js
+++ osa-2/maiden-tiedot/src/App.js
@@ -1,18 +1,37 @@
 import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [countries, setCountries] = useState([]);
 
-const Input = () => {
-  return(
-    <div>
-    <p>Find countries</p>
-    <input></input>
+  
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(res => {
+        console.log('Promise fullfilled')
+        setCountries(res.data);
+      })
+  }
+
+  useEffect(hook, [])
+  console.log('render', countries.length, 'countries with info..');
+  
+
+  const Filter = ({value, onChange}) => {
+    return(
+    <div>Filter shown with
+      <input>
+      </input>
     </div>
-  )
-}
+    )
+  }
+
+
 
 
 
@@ -21,7 +40,7 @@ const Input = () => {
 
  return (
    <div>
-    <Input/>
+     <Filter/>
    </div>
  )
 }
