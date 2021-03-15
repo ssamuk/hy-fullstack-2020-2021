d5df5f936e3476c0250c4dbf910a4b63b0eb2981
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2021-03-15 18:38:16 +0200

    Lodash library

--- osa-4/utils/list_helper.js
+++ osa-4/utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
   
@@ -9,9 +9,26 @@ const totalLikes = (blogs) => {
     }, 0)
   )
 }
-
+const favouriteBlog = (blogs) => {
+  let liked = blogs.reduce((blog, currentValue, index) => {
+    if (index === 0) {
+      return currentValue
+    }
+    return blog.likes > currentValue.likes ? blog : currentValue
+  })
+  return {
+    title: liked.title,
+    author: liked.author,
+    likes: liked.likes
+  }
+}
+const mostBlogs = (blogs) => {
+  //  Most blogs code here
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog,
+  mostBlogs
 }
